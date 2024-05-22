fcde5248b309e031abd53a3e91afe97b4319fd91
Jamie McStay <jamiemcstay@hotmail.com>
2024-05-22 20:20:39 +0000

    Create checkroll function and add if statement to compare highroll result

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,64 +3,79 @@
 document.addEventListener('DOMContentLoaded', function () {
     //hide piggy-bank and score area on screen onload
     let scoreArea = document.getElementById('score-area');
+    scoreArea.style.display = 'none';
 
     let piggyBank = document.getElementById('piggy-bank-container');
+    piggyBank.style.display = 'none';
 
     //increase size of main section to take up surplace space 
     let mainSection = document.getElementById('main-section');
+    mainSection.style.height = '90%';
     
     let circle = document.getElementById('main-section-circle');
 
-    scoreArea.style.display = 'none';
-    piggyBank.style.display = 'none';
-    mainSection.style.height = '90%';
     
     function clickToStart() {
         scoreArea.style.display = 'flex';
         piggyBank.style.display = 'flex';
         mainSection.style.height = '72%';
+
+        let heading = document.getElementById('h1-large-screens');
+        heading.textContent = "Click for high roll";
         
-        runGame(); 
+        circle.removeEventListener('click', clickToStart);
+        
+        circle.addEventListener('click', highRoll);
 
     }
 
-    circle.addEventListener('click', clickToStart);
+
+        circle.addEventListener('click', clickToStart); 
 
 });
 
-
 //function for rolling dice
+
 function rollDice() {
     return Math.floor(Math.random() * 6) +1;
 };
 
-
-//Function for running the game
-
-function runGame() {
-    console.log("You're running the game");
-    let heading = document.getElementById('h1-large-screens');
-    heading.textContent = "Click for high roll";
-
-    let circle = document.getElementById('main-section-circle');
-    circle.addEventListener('click', highRoll); 
-
-}
-
-//function for checking high roll
-
 function highRoll() {
+
     console.log('Youre in the high roll');
     let num1 = rollDice();
     let num2 = rollDice();
 
     let highRollResult = [num1, num2]
-    console.log(highRollResult); 
+    if(num1 > num2) {
+        console.log('yup'); 
+    }
+    checkRoll(highRollResult);
+    
+    let circle = document.getElementById('main-section-circle');
+    circle.removeEventListener('click', highRoll); 
+    
+};
+
+
+
+//Function for running the game
+
+function runGame() { 
+    
 }
+
+//function for checking high roll
 
 //Function for rolling three dice
 
-//Function for checking three dice rolls
+//Function for checking the roll of dice
+
+function checkRoll(highRollResult) {
+    if(highRollResult[0] > highRollResult[1]) {
+    } else { 
+    }
+};
 
 //Function for finding winnner of round/game
 
