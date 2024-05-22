8d92b3c45774567b16faafafb06263a7da86173f
Jamie McStay <jamiemcstay@hotmail.com>
2024-05-22 07:13:57 +0000

    Change angles of dice in circle and add lets play dice h1

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+//Function for screen on loading
+
+document.addEventListener('DomContentLoaded', function () {
+    
+})
+
+//Function for running the game and the rounds
+
+//function for high roll
+
+//function for checking high roll
+
+//Function for rolling three dice
+
+//Function for checking three dice rolls
+
+//Function for finding winnner of round/game
+
+//Function for increasing stakes and updating piggy bank
+
+
