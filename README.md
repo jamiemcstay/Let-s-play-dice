5531ae646fe99438aa0a0710ff865549da693160
Jamie McStay <jamiemcstay@hotmail.com>
2024-05-22 14:19:13 +0000

    Add structure for functions in js file

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
+https://www.w3schools.com/howto/howto_js_media_queries.asp#:~:text=Using%20Media%20Queries%20With%20JavaScript&text=matchMedia()%20method%20returns%20a,%2Dwidth%2C%20orientation%2C%20etc.
+
+
 ![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+
 
 Welcome,
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 //Function for screen on loading
 
-document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('score-area').style.display = 'none';
-    document.getElementById('piggy-bank-container').display = 'none';
-    document.getElementById('')
+    document.getElementById('piggy-bank-container').style.display = 'none';
+    document.getElementById('main-section').style.height = '90%'; 
+    document.getElementById('main-section-circle').style.top = '165px';
+    document.getElementbyId('')
+
 });
 
 //Function for running the game and the rounds
