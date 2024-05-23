117a33a4ffdf1216dfd98be7719f0d0d0c57324a
Jamie McStay <jamiemcstay@hotmail.com>
2024-05-23 11:14:16 +0000

    Add timeout in high roll and change remove all but middle dice for high roll

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,17 @@ document.addEventListener('DOMContentLoaded', function () {
         let heading = document.getElementById('h1-large-screens');
         heading.textContent = "Click for high roll";
 
+        let diceOne = document.getElementById('die1');
+        diceOne.style.display = 'none'; 
+
+        let diceTwoChange = document.querySelector('.fa-solid.fa-dice-five.die');
+        if (diceTwoChange) {
+        diceTwoChange.setAttribute('class', 'fa-solid fa-dice-five fa-beat die');
+        }
+        
+        let diceThree = document.getElementById('die3'); 
+        diceThree.style.display = 'none';      
+
         circle.removeEventListener('click', clickToStart);
 
         circle.addEventListener('click', highRoll);
@@ -34,53 +45,70 @@ document.addEventListener('DOMContentLoaded', function () {
     function rollDice() {
         return Math.floor(Math.random() * 6) + 1;
     }
-    
+
     function highRoll() {
-    
+
         console.log('Youre in the high roll');
         // let heading = document.getElementById('h1-large-screens'); 
         let circle = document.getElementById('main-section-circle');
         circle.removeEventListener('click', highRoll);
-    
+
         let num1 = rollDice();
         let num2 = rollDice();
-    
-        let highRollResult = [num1, num2]; 
-    
+
+        let highRollResult = [num1, num2];
+        // changeDice(); 
         checkHighRoll(highRollResult);
-    
+
+        setTimeout (function() {
+            let diceTwoChange = document.querySelector('.fa-solid.fa-dice-five.fa-beat.die');
+            if (diceTwoChange) {
+                diceTwoChange.setAttribute('class', 'fa-solid fa-dice-five die');
+            }
+        }, 200);
+
+
         return highRollResult;
 
     }
 
+
+    // function changeDice() {
+    //     let diceOne = document.getElementById('die1');
+    //     diceOne.style.display = 'none'; 
+    // }
+
     function checkHighRoll(highRollResult) {
 
         let heading = document.getElementById('h1-large-screens');
-        heading.style.fontSize = '300%'; 
+        heading.style.fontSize = '300%';
 
         if (highRollResult[0] > highRollResult[1]) {
 
             heading.textContent = `You rolled a ${highRollResult[0]}`;
-            
-            setTimeOut(function() {
-                heading.textContent = `Computer rolled a ${highRollResult[1]}`;
 
+            setTimeout(function () {
+                heading.textContent = `Computer rolled a ${highRollResult[1]}`;
             }, 2000);
 
-            setTimeOut(function() {
+            setTimeout(function () {
                 heading.textContent = "Set the stakes";
                 setTheStakes();
-            }, 2000); 
+            }, 4000);
 
         } else {
-            heading.textContent  = `Computer rolled a ${highRollResult[1]}`;
+            heading.textContent = `You rolled a ${highRollResult[0]}`; 
 
-            setTimeout(function() {
+            setTimeout(function () {
+                heading.textContent = `Computer rolled a ${highRollResult[1]}`; 
+            }, 2000); 
+
+            setTimeout(function () {
                 heading.textContent = `The stakes are stakes `;
-                computerRoll(); 
-            }, 2000);
+                computerRoll();
+            }, 4000);
 
-        }   
+        }
     }
 
     function computerStakes() {
@@ -88,7 +116,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     }
 
-        //function for setting the stakes 
+    //function for setting the stakes 
     function setTheStakes() {
         console.log("Set the stakes");
     }
@@ -96,26 +124,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //Function for running the game
     function runGame() {
-    
-        console.log("You're in the game"); 
-    
+
+        console.log("You're in the game");
+
         let circle = document.getElementById('main-section-circle');
-    
-        let heading = document.getElementById('h1-large-screens');       
-    
+
+        let heading = document.getElementById('h1-large-screens');
+
     }
-    
-
-}); 
 
 
-
-
-    
-    
-
-    
-    
-
-
-
+});
