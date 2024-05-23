773b41a33d0b104bea14104b3e85b86bddeb2bb3
Jamie McStay <jamiemcstay@hotmail.com>
2024-05-23 07:24:53 +0000

    Put code into higher order function and add logic for checkroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,55 +30,88 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 
+    function rollDice() {
+        return Math.floor(Math.random() * 6) + 1;
+    }
+    
+    function highRoll() {
+    
+        console.log('Youre in the high roll');
+        let heading = document.getElementById('')
+        let circle = document.getElementById('main-section-circle');
+        circle.removeEventListener('click', highRoll);
+    
+        let num1 = rollDice();
+        let num2 = rollDice();
+    
+        let highRollResult = [num1, num2]; 
+    
+        checkHighRoll(highRollResult);
+    
+        return highRollResult;
+
+            function checkHighRoll(highRollResult) {
+
+                let heading = document.getElementById('h1-large-screens');
+                
+        
+                if (highRollResult[0] > highRollResult[1]) {
+                    console.log('Your number is higher, leaving checkHighRoll');
+    
+                    heading.textContent = `You rolled a ${highRollResult[0]} Computer rolled a ${highRollResult[1]}, set the stakes`;
+
+                    playerRoll(); 
+                
+                } else {
+                    heading.textContent  = `Computer rolled a ${highRollResult[1]}, the stakes are stakes, leaving checkHighRoll`;
+                    computerRoll();
+                }   
+        }
+    
+    }
+    
+    
+    
+    //Function for running the game
+    
+    function runGame() {
+    
+        console.log("You're in the game"); 
+    
+        let circle = document.getElementById('main-section-circle');
+    
+        let heading = document.getElementById('h1-large-screens');
+    
+        
+        
+    
+    }
+    
+    //function for checking high roll
+    
+    //Function for rolling three dice
+    
+    //Function for checking the roll of dice
+    
+    
+    function playerRoll() {
+    
+        console.log("You're in playerRoll"); 
+    
+    };
+
+    
+    function computerRoll() {
+        console.log("You're in computer roll");
+    }
+
+
+    //Function for finding winnner of round/game
+    
+    //Function for increasing stakes and updating piggy bank
+
+
     circle.addEventListener('click', clickToStart);
 
 });
 
-//function for rolling dice
-
-function rollDice() {
-    return Math.floor(Math.random() * 6) + 1;
-};
-
-function highRoll() {
-
-    console.log('Youre in the high roll');
-    let num1 = rollDice();
-    let num2 = rollDice();
-
-    let highRollResult = [num1, num2]
-    if (num1 > num2) {
-        console.log('yup');
-    }
-    checkRoll(highRollResult);
-
-    let circle = document.getElementById('main-section-circle');
-    circle.removeEventListener('click', highRoll);
-
-};
-
-
-
-//Function for running the game
-
-function runGame() {
-
-}
-
-//function for checking high roll
-
-//Function for rolling three dice
-
-//Function for checking the roll of dice
-
-function checkRoll(highRollResult) {
-    if (highRollResult[0] > highRollResult[1]) {
-        console.log('Your number is higher');
-    } else {
-        console.log('shit one');
-    }
-};
-
-//Function for finding winnner of round/game
-
-//Function for increasing stakes and updating piggy bank
